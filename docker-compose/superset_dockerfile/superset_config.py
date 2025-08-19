f21e649ae6c805c46e31cb0da60478c7807510da
Alya-DE <164178017+Alya-DE@users.noreply.github.com>
2025-08-19 17:04:56 +0300

    Update superset_config.py

--- docker-compose/superset_dockerfile/superset_config.py
+++ docker-compose/superset_dockerfile/superset_config.py
@@ -1,13 +1,13 @@
 import os    
 from datetime import timedelta
-from superset.config import *
-
+ 
 # Основные настройки
-SECRET_KEY = 'YOUR_SECRET_KEY' # YOUR_SECRET_KEY сгенерируем заранее с помощью команды openssl rand -base64 42
-
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://airflow:airflow@postgres:5432/hh_data_engineer' 
+SECRET_KEY = os.getenv('SUPERSET_SECRET_KEY', 'mYSECRETkey_123')
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://airflow:airflow@postgres:5432/superset_meta' 
+THUMBNAILS_ENABLED = True
+THUMBNAILS_SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI
 
 # Настройки безопасности
 CSRF_ENABLED = True  # Включим защиту CSRF
-WTF_CSRF_ENABLED = True  # Включим CSRFзащиту для форм, реализованных с использованием Flask-WTF
+WTF_CSRF_ENABLED = True  # Включим CSRF защиту для форм, реализованных с использованием Flask-WTF
 WTF_CSRF_TIME_LIMIT = None  # Установим лимит времени для токена CSRF 
