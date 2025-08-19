063f2d7a10619c364c67e06f4733a483f955f815
Alya-DE <164178017+Alya-DE@users.noreply.github.com>
2025-08-19 17:06:12 +0300

    Update superset-init.sh

--- docker-compose/superset_dockerfile/superset-init.sh
+++ docker-compose/superset_dockerfile/superset-init.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-
+ 
 # Инициализация базы данных Superset
 superset db upgrade
-
+ 
 # Создание учетной записи администратора
-superset fab create-admin --username "${ADMIN_USERNAME}" --firstname Superset --lastname Admin --email "${ADMIN_EMAIL}" --password "${ADMIN_PASSWORD}"
-
+superset fab create-admin \
+    --username "${ADMIN_USERNAME}" \
+    --firstname Superset \
+    --lastname Admin \
+    --email "${ADMIN_EMAIL}" \
+    --password "${ADMIN_PASSWORD}"
+ 
+superset init
+ 
+# Ожидание пока Superset поднимется
+sleep 90
+ 
 # Загрузка примерных данных (если необходимо)
 # superset load_examples
-
-# Настройка Superset
-superset init
-
-# Запуск Superset
-superset run -p 8088 -h 0.0.0.0 --with-threads --reload --debugger
-
-# Ждем 60 секунд перед добавлением подключения к базе данных
-sleep 60
-
-# Добавление подключения к базе данных PostgreSQL
+ 
+# Добавление подключения к внешней базе данных PostgreSQL
 superset dbs add \
   --database-name hh_data_engineer \
   --sqlalchemy-uri postgresql+psycopg2://airflow:airflow@postgres:5432/hh_data_engineer \
   --configuration-method sql \
-  --username admin \
-  --password password
